8ba91e5e2ae733b1cf76995770742e737326a59b
Josh Cardif <josh.cardif14@outlook.com>
2018-10-05 19:02:33 +0300

    Add Model

---
+++ AutoChat/Adapters/ChatListViewAdapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.Views;
+using Android.Widget;
+using Object = Java.Lang.Object;
+
+namespace AutoChat.Adapters
+{
+    public class ChatListViewAdapter : BaseAdapter
+    {
+        public override Object GetItem(int position)
+        {
+            return position;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Count { get; }
+    }
+}
---
+++ AutoChat/Models/AutoChatItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AutoChat.Models
+{
+    public enum Status
+    {
+        Sent, Delivered
+    }
+
+    public enum UserType
+    {
+        Other, Self
+    }
+    public class AutoChatItem
+    {
+        public UserType UserType { get; set; }
+        public string Time { get; set; }
+        public string Message { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
